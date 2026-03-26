b94516c277e376e7018f97d7fc5136a6e0581d6d
지혁 박 <jhboy0714@gmail.com>
2026-03-26 11:41:31 +0900

    과제1 코딩과 테스트 완료 라벨, 2개의 텍스트박스, 20개의 버튼을 배치함 20개의 버튼 중 숫자 버튼을 눌렀을 때 각각 숫자가 텍스트박스에 입력되도록 기능을 추가 +버튼을 눌렀을 때 하나의 텍스트박스에는 그대로 입력되고 다른 하나의 텍스트박스에는 입력된 숫자가 지워지는 기능 추가 =버튼을 눌렀을 때 입력된 내용들을 받아 계산하고 결과를 출력, 하나의 텍스트박스에는 수식과 결과 전부, 다른 하나의 텍스트박스에는 결과만 표시되도록 기능을 추가

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        ///  필수 디자이너 변수입니다.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        ///  사용 중인 모든 리소스를 정리합니다.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">관리되는 리소스를 해제해야 하면 true, 그렇지 않으면 false입니다.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -23,17 +23,315 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        ///  디자이너 지원에 필요한 메서드입니다 - 코드 편집기로
+        ///  이 메서드의 내용을 수정하지 마세요.
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblTitle = new Label();
+            txtInput = new TextBox();
+            txtOutput = new TextBox();
+            btnNumber0 = new Button();
+            btnNumber2 = new Button();
+            btnNumber1 = new Button();
+            btnNumber3 = new Button();
+            btnNumber6 = new Button();
+            btnNumber9 = new Button();
+            btnNumber5 = new Button();
+            btnNumber8 = new Button();
+            btnNumber4 = new Button();
+            btnNumber7 = new Button();
+            btnConversion = new Button();
+            btnDecimalPoint = new Button();
+            btnClearEntry = new Button();
+            btnClear = new Button();
+            btnDelete = new Button();
+            btnOperatorDivide = new Button();
+            btnOperatorMultiply = new Button();
+            btnOperatorSubtraction = new Button();
+            btnOperatorAdd = new Button();
+            btnOpeartorEqual = new Button();
+            SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("맑은 고딕", 20F);
+            lblTitle.ForeColor = Color.DodgerBlue;
+            lblTitle.Location = new Point(26, 51);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(288, 46);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "Simple Calculator";
+            // 
+            // txtInput
+            // 
+            txtInput.Location = new Point(26, 125);
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(387, 27);
+            txtInput.TabIndex = 1;
+            txtInput.TextChanged += txt_TextChanged;
+            // 
+            // txtOutput
+            // 
+            txtOutput.Location = new Point(26, 158);
+            txtOutput.Name = "txtOutput";
+            txtOutput.Size = new Size(387, 27);
+            txtOutput.TabIndex = 2;
+            txtOutput.TextChanged += textBox2_TextChanged;
+            // 
+            // btnNumber0
+            // 
+            btnNumber0.Location = new Point(135, 417);
+            btnNumber0.Name = "btnNumber0";
+            btnNumber0.Size = new Size(81, 40);
+            btnNumber0.TabIndex = 5;
+            btnNumber0.Text = "0";
+            btnNumber0.UseVisualStyleBackColor = true;
+            btnNumber0.Click += NumberButton_Click;
+            // 
+            // btnNumber2
+            // 
+            btnNumber2.Location = new Point(135, 371);
+            btnNumber2.Name = "btnNumber2";
+            btnNumber2.Size = new Size(81, 40);
+            btnNumber2.TabIndex = 6;
+            btnNumber2.Text = "2";
+            btnNumber2.UseVisualStyleBackColor = true;
+            btnNumber2.Click += NumberButton_Click;
+            // 
+            // btnNumber1
+            // 
+            btnNumber1.Location = new Point(48, 371);
+            btnNumber1.Name = "btnNumber1";
+            btnNumber1.Size = new Size(81, 40);
+            btnNumber1.TabIndex = 7;
+            btnNumber1.Text = "1";
+            btnNumber1.UseVisualStyleBackColor = true;
+            btnNumber1.Click += NumberButton_Click;
+            // 
+            // btnNumber3
+            // 
+            btnNumber3.Location = new Point(222, 371);
+            btnNumber3.Name = "btnNumber3";
+            btnNumber3.Size = new Size(81, 40);
+            btnNumber3.TabIndex = 8;
+            btnNumber3.Text = "3";
+            btnNumber3.UseVisualStyleBackColor = true;
+            btnNumber3.Click += NumberButton_Click;
+            // 
+            // btnNumber6
+            // 
+            btnNumber6.Location = new Point(222, 325);
+            btnNumber6.Name = "btnNumber6";
+            btnNumber6.Size = new Size(81, 40);
+            btnNumber6.TabIndex = 9;
+            btnNumber6.Text = "6";
+            btnNumber6.UseVisualStyleBackColor = true;
+            btnNumber6.Click += NumberButton_Click;
+            // 
+            // btnNumber9
+            // 
+            btnNumber9.Location = new Point(222, 279);
+            btnNumber9.Name = "btnNumber9";
+            btnNumber9.Size = new Size(81, 40);
+            btnNumber9.TabIndex = 10;
+            btnNumber9.Text = "9";
+            btnNumber9.UseVisualStyleBackColor = true;
+            btnNumber9.Click += NumberButton_Click;
+            // 
+            // btnNumber5
+            // 
+            btnNumber5.Location = new Point(135, 325);
+            btnNumber5.Name = "btnNumber5";
+            btnNumber5.Size = new Size(81, 40);
+            btnNumber5.TabIndex = 11;
+            btnNumber5.Text = "5";
+            btnNumber5.UseVisualStyleBackColor = true;
+            btnNumber5.Click += NumberButton_Click;
+            // 
+            // btnNumber8
+            // 
+            btnNumber8.Location = new Point(135, 279);
+            btnNumber8.Name = "btnNumber8";
+            btnNumber8.Size = new Size(81, 40);
+            btnNumber8.TabIndex = 12;
+            btnNumber8.Text = "8";
+            btnNumber8.UseVisualStyleBackColor = true;
+            btnNumber8.Click += NumberButton_Click;
+            // 
+            // btnNumber4
+            // 
+            btnNumber4.Location = new Point(48, 325);
+            btnNumber4.Name = "btnNumber4";
+            btnNumber4.Size = new Size(81, 40);
+            btnNumber4.TabIndex = 13;
+            btnNumber4.Text = "4";
+            btnNumber4.UseVisualStyleBackColor = true;
+            btnNumber4.Click += NumberButton_Click;
+            // 
+            // btnNumber7
+            // 
+            btnNumber7.Location = new Point(48, 279);
+            btnNumber7.Name = "btnNumber7";
+            btnNumber7.Size = new Size(81, 40);
+            btnNumber7.TabIndex = 14;
+            btnNumber7.Text = "7";
+            btnNumber7.UseVisualStyleBackColor = true;
+            btnNumber7.Click += NumberButton_Click;
+            // 
+            // btnConversion
+            // 
+            btnConversion.Location = new Point(48, 417);
+            btnConversion.Name = "btnConversion";
+            btnConversion.Size = new Size(81, 40);
+            btnConversion.TabIndex = 15;
+            btnConversion.Text = "+/-";
+            btnConversion.UseVisualStyleBackColor = true;
+            // 
+            // btnDecimalPoint
+            // 
+            btnDecimalPoint.Location = new Point(222, 417);
+            btnDecimalPoint.Name = "btnDecimalPoint";
+            btnDecimalPoint.Size = new Size(81, 40);
+            btnDecimalPoint.TabIndex = 16;
+            btnDecimalPoint.Text = ".";
+            btnDecimalPoint.UseVisualStyleBackColor = true;
+            // 
+            // btnClearEntry
+            // 
+            btnClearEntry.Location = new Point(48, 233);
+            btnClearEntry.Name = "btnClearEntry";
+            btnClearEntry.Size = new Size(81, 40);
+            btnClearEntry.TabIndex = 17;
+            btnClearEntry.Text = "CE";
+            btnClearEntry.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(135, 233);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(81, 40);
+            btnClear.TabIndex = 18;
+            btnClear.Text = "C";
+            btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(222, 233);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(81, 40);
+            btnDelete.TabIndex = 19;
+            btnDelete.Text = "del";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnOperatorDivide
+            // 
+            btnOperatorDivide.Location = new Point(309, 233);
+            btnOperatorDivide.Name = "btnOperatorDivide";
+            btnOperatorDivide.Size = new Size(81, 40);
+            btnOperatorDivide.TabIndex = 20;
+            btnOperatorDivide.Text = "÷";
+            btnOperatorDivide.UseVisualStyleBackColor = true;
+            // 
+            // btnOperatorMultiply
+            // 
+            btnOperatorMultiply.Location = new Point(309, 279);
+            btnOperatorMultiply.Name = "btnOperatorMultiply";
+            btnOperatorMultiply.Size = new Size(81, 40);
+            btnOperatorMultiply.TabIndex = 21;
+            btnOperatorMultiply.Text = "X";
+            btnOperatorMultiply.UseVisualStyleBackColor = true;
+            // 
+            // btnOperatorSubtraction
+            // 
+            btnOperatorSubtraction.Location = new Point(309, 325);
+            btnOperatorSubtraction.Name = "btnOperatorSubtraction";
+            btnOperatorSubtraction.Size = new Size(81, 40);
+            btnOperatorSubtraction.TabIndex = 22;
+            btnOperatorSubtraction.Text = "-";
+            btnOperatorSubtraction.UseVisualStyleBackColor = true;
+            // 
+            // btnOperatorAdd
+            // 
+            btnOperatorAdd.Location = new Point(309, 371);
+            btnOperatorAdd.Name = "btnOperatorAdd";
+            btnOperatorAdd.Size = new Size(81, 40);
+            btnOperatorAdd.TabIndex = 23;
+            btnOperatorAdd.Text = "+";
+            btnOperatorAdd.UseVisualStyleBackColor = true;
+            btnOperatorAdd.Click += btnOperatorAdd_Click;
+            // 
+            // btnOpeartorEqual
+            // 
+            btnOpeartorEqual.BackColor = SystemColors.Highlight;
+            btnOpeartorEqual.ForeColor = SystemColors.ButtonFace;
+            btnOpeartorEqual.Location = new Point(309, 417);
+            btnOpeartorEqual.Name = "btnOpeartorEqual";
+            btnOpeartorEqual.Size = new Size(81, 40);
+            btnOpeartorEqual.TabIndex = 24;
+            btnOpeartorEqual.Text = "=";
+            btnOpeartorEqual.UseVisualStyleBackColor = false;
+            btnOpeartorEqual.Click += btnOperatorEqual_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(441, 512);
+            Controls.Add(btnOpeartorEqual);
+            Controls.Add(btnOperatorAdd);
+            Controls.Add(btnOperatorSubtraction);
+            Controls.Add(btnOperatorMultiply);
+            Controls.Add(btnOperatorDivide);
+            Controls.Add(btnDelete);
+            Controls.Add(btnClear);
+            Controls.Add(btnClearEntry);
+            Controls.Add(btnDecimalPoint);
+            Controls.Add(btnConversion);
+            Controls.Add(btnNumber7);
+            Controls.Add(btnNumber4);
+            Controls.Add(btnNumber8);
+            Controls.Add(btnNumber5);
+            Controls.Add(btnNumber9);
+            Controls.Add(btnNumber6);
+            Controls.Add(btnNumber3);
+            Controls.Add(btnNumber1);
+            Controls.Add(btnNumber2);
+            Controls.Add(btnNumber0);
+            Controls.Add(txtOutput);
+            Controls.Add(txtInput);
+            Controls.Add(lblTitle);
+            Name = "Form1";
+            Text = "Calculator";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblTitle;
+        private TextBox txtInput;
+        private TextBox txtOutput;
+        private Button btnNumber0;
+        private Button btnNumber2;
+        private Button btnNumber3;
+        private Button btnNumber6;
+        private Button btnNumber9;
+        private Button btnNumber1;
+        private Button btnNumber5;
+        private Button btnNumber8;
+        private Button btnNumber4;
+        private Button btnNumber7;
+        private Button btnConversion;
+        private Button btnDecimalPoint;
+        private Button btnClearEntry;
+        private Button btnClear;
+        private Button btnDelete;
+        private Button btnOperatorDivide;
+        private Button btnOperatorMultiply;
+        private Button btnOperatorSubtraction;
+        private Button btnOperatorAdd;
+        private Button btnOpeartorEqual;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,168 @@ namespace SimpleCalculator
         {
             InitializeComponent();
         }
+
+        private void btnOperatorEqual_Click(object sender, EventArgs e)
+        {
+            string expr = txtInput.Text;
+            if (string.IsNullOrWhiteSpace(expr))
+                return;
+
+            // 수식에 이미 '='가 포함되어 있으면 아무 작업도 하지 않습니다.
+            if (expr.Contains('='))
+                return;
+
+            // 연산자 찾기 (단일 연산자 수식 A+B 같은 형식을 지원)
+            int opIndex = -1;
+            char op = '\0';
+            for (int i = 0; i < expr.Length; i++)
+            {
+                char c = expr[i];
+                if (c == '+' || c == '-' || c == 'X' || c == '÷')
+                {
+                    opIndex = i;
+                    op = c;
+                    break;
+                }
+            }
+
+            if (opIndex <= 0 || opIndex >= expr.Length - 1)
+            {
+                // 잘못된 수식 (연산자가 없거나 피연산자가 없음)
+                return;
+            }
+
+            string left = expr.Substring(0, opIndex);
+            string right = expr.Substring(opIndex + 1);
+
+            if (!double.TryParse(left, out double a) || !double.TryParse(right, out double b))
+            {
+                // 숫자로 변환할 수 없음
+                return;
+            }
+
+            double result = 0;
+            bool valid = true;
+            switch (op)
+            {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case 'X':
+                    result = a * b;
+                    break;
+                case '÷':
+                    if (b == 0)
+                        valid = false; // 0으로 나누기 오류
+                    else
+                        result = a / b;
+                    break;
+                default:
+                    valid = false;
+                    break;
+            }
+
+            if (!valid)
+            {
+                txtOutput.Text = "Error";
+                return;
+            }
+
+            string resultStr;
+            // 결과가 정수이면 소수점 없이 표시
+            if (Math.Abs(result % 1) < 1e-10)
+                resultStr = ((long)result).ToString();
+            else
+                resultStr = result.ToString();
+
+            // txtInput에 '=결과'를 추가하고 txtOutput에는 결과만 표시
+            txtInput.Text = expr + "=" + resultStr;
+            txtOutput.Text = resultStr;
+        }
+
+        private void txt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void NumberButton_Click(object sender, EventArgs e)
+        {
+            if (sender is Button b)
+            {
+                string digit = b.Text;
+                // If the textbox currently contains only a single '0', replace it with the new digit.
+                // Otherwise append the digit. Handle empty text as append (becomes the digit).
+                void AppendOrReplace(TextBox tb)
+                {
+                    if (string.IsNullOrEmpty(tb.Text) || tb.Text == "0")
+                    {
+                        tb.Text = digit;
+                    }
+                    else
+                    {
+                        tb.Text += digit;
+                    }
+                }
+
+                AppendOrReplace(txtInput);
+                AppendOrReplace(txtOutput);
+            }
+        }
+
+        private void btnOperatorAdd_Click(object sender, EventArgs e)
+        {
+            string op = "+";
+
+            // If last character of txtInput is already an operator, replace it.
+            if (!string.IsNullOrEmpty(txtInput.Text))
+            {
+                char last = txtInput.Text[txtInput.Text.Length - 1];
+                if (last == '+' || last == '-' || last == 'X' || last == '÷')
+                {
+                    txtInput.Text = txtInput.Text.Substring(0, txtInput.Text.Length - 1) + op;
+                }
+                else
+                {
+                    txtInput.Text += op;
+                }
+            }
+            else
+            {
+                // If input empty and user presses +, treat it as starting with 0+
+                txtInput.Text = "0" + op;
+            }
+
+            // For txtOutput, keep only the last operand (text after the last operator).
+            string input = txtInput.Text;
+            int lastOpIndex = -1;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                char c = input[i];
+                if (c == '+' || c == '-' || c == 'X' || c == '÷')
+                {
+                    lastOpIndex = i;
+                    break;
+                }
+            }
+
+            if (lastOpIndex >= 0 && lastOpIndex < input.Length - 1)
+            {
+                txtOutput.Text = input.Substring(lastOpIndex + 1);
+            }
+            else
+            {
+                // If operator is at the end or not found, clear output to allow next number entry
+                txtOutput.Text = string.Empty;
+            }
+        }
     }
 }
